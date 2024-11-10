e3ed3c3e658f2bba94698f52cf9ad55d16f0b3b7
Ethan Dirkes <eldirkes@gmail.com>
2024-11-10 16:18:11 -0600

    Fixed inability for sequence game to pick yellow and timing of flashes

--- games/sequence/sequence.js
+++ games/sequence/sequence.js
@@ -4,22 +4,34 @@
     Made by: Ethan Dirkes
 
     Created 11/9/2024
-
-    Known bugs:
-        - Yellow color never gets selected for sequence
-        - showSequence doesn't fade out when same color is selected back to back
-            (i.e. red->red shows a long flash of red rather than two short flashes)
-    Tweaks needed:
-        - Add comments
-        - Timing needs to be adjusted
+    Edited 11/10/2024 (Ethan):
+        - Added comments
+        - Fixed bug where yellow was never picked for sequence
+        - Fixed timing of flashes so repeated colors now do seperate flashes
+            instead of a long flash
+        - Added time after last flash of sequence before player can start guessing
+    Preconditions:
+        Only inputs are mouse clicks on the color buttons
+    Postconditions:
+        Only return value is the final score
+    Errors:
+        None
+    Side effects:
+        None
+    Invariants:
+        - Colors present in game
+        - Values for game states
+        - References to DOM elements
+    Known faults:
+        None
 */
 
-const COLORS = ['blue', 'red', 'green', 'blue'];
-let sequence = []
-let level = 0;
-let num_guesses = 0;
-let state = undefined;
-let mouseover = false;
+const COLORS = ['blue', 'red', 'green', 'yellow'];    // Colors for game
+let sequence = []                                   // Generated sequence for current game
+let level = 0;                                      // Level the player is at
+let num_guesses = 0;                                // The number of guesses the player has for the current round
+let state = undefined;                              // State of the game
+let mouseover = false;                              // If the mouse is over a button
 
 // Enum for game states
 const STATES = Object.freeze({
@@ -30,83 +42,105 @@ const STATES = Object.freeze({
     GAMEOVER:   4
 });
 
+// Get DOM elements from page
 const START_BUTTON = document.getElementById('start');
 const GAMEBOARD = document.getElementById('gameboard');
-const COLOR_ELEMENTS = document.querySelectorAll('.color');
+const COLOR_ELEMENTS = document.querySelectorAll('.color'); // Select all color button elements
 const RESULTS = document.getElementById('results');
 
+// Listen for when game is started
 START_BUTTON.addEventListener('click', startGame);
 
+// Initialize the game
 function startGame() {
-    state = STATES.START;
+    state = STATES.START;   // Set state
+
+    // Disable button
     START_BUTTON.classList.remove('btn');
     START_BUTTON.classList.add('btn-d')
-    RESULTS.style.visibility="visible";
-    sequence = [];
-    num_guesses = 0;
-    level = 0;
-    state = STATES.NEXTRND;
-    nextRound();
+    RESULTS.style.visibility="visible"; // Show results messages
+    sequence = [];              // Reset sequence
+    level = 0;                  // Reset level
+    state = STATES.NEXTRND;     // Change to next state
+    nextRound();                // Begin first round
 }
 
+// Begin next round
 function nextRound() {
-    level++;
-    num_guesses = 0;
-    const next_color = COLORS[Math.floor(Math.random() * COLORS.length)];
-    sequence.push(next_color);
+    level++;            // Increase level
+    num_guesses = 0;    // Reset number of guesses
+    const next_color = COLORS[Math.floor(Math.random() * COLORS.length)];   // Pick random color for sequence
+    sequence.push(next_color);  // Push that color to the sequence array
 
-    COLOR_ELEMENTS.forEach(color => color.classList.remove('active'));
-    RESULTS.innerHTML="Level " + level;
-    showSequence();
+    COLOR_ELEMENTS.forEach(color => color.classList.remove('active'));  // Remove active tag from each color button
+    RESULTS.innerHTML="Level " + level; // Display current level
+    showSequence(); // Show the generated sequence
 }
 
+// Show the current sequence in full
 function showSequence() {
-    state = STATES.SHOWSEQ;
-    let i = 0;
+    state = STATES.SHOWSEQ; // Update state
+    let i = 0;  // i: index of sequence array to display
+
+    // Display a color over a set interval
     const interval = setInterval(() =>{
+        // Get the ID of the color element to flash and add the 'active' tag to it
         const COLOR = document.getElementById(sequence[i]);
         COLOR.classList.add('active');
-        setTimeout(() => COLOR.classList.remove('active'), Math.max(2000/sequence.length, 100));
-        i++;
+
+        // Remove 'active' tag after a time
+        setTimeout(() => COLOR.classList.remove('active'), Math.max(1000/Math.sqrt(sequence.length), 100));
+        i++;    // Increase index counter
+
+        // Break out of interval loop if end of sequence reached
         if (i >= sequence.length) {
-            clearInterval(interval);
-            state = STATES.PLAYERSEQ;
+            setTimeout(() => {
+                clearInterval(interval);
+                state = STATES.PLAYERSEQ;   // Change state
+            }, Math.max(1000/Math.sqrt(sequence.length), 100));
         }
-    }, Math.max(2000/sequence.length, 100));
+    }, Math.max(2000/Math.sqrt(sequence.length), 200));
 }
 
+// Add event listeners to each color button
 COLOR_ELEMENTS.forEach(colorElement => {
-    colorElement.addEventListener('click', (event) => {
-        if (state != STATES.PLAYERSEQ) return;
+    colorElement.addEventListener('click', (event) => {     // Event for when the button is clicked
+        if (state != STATES.PLAYERSEQ) return;  // If not in player guessing state, return
 
-        num_guesses++;
+        num_guesses++;  // Increase number of guesses
+
+        // Get ID of button clicked and check if click was correct
         const COLOR = event.target.id;
         event.target.classList.remove('hover');
-        event.target.classList.add('active');
         let res = checkSequence(COLOR);
 
+        // Do as result of sequence check
         switch(res) {
+            // Incorrect, game over
             case 0:
-                event.target.classList.remove('active');
                 gameOver();
                 break;
+            // Correct, end of sequence, next round
             case 1:
                 state = STATES.NEXTRND;
                 RESULTS.innerHTML = "Correct! Beginning next level...";
                 setTimeout(nextRound(), 1000);
-                event.target.classList.remove('active')
                 break;
+            // Correct
             default:
-                event.target.classList.remove('active');
                 event.target.classList.add('hover');
         }
     });
+
+    // Event for if mouse is over, add hover tag if true
     colorElement.addEventListener('mouseover', (event) =>{
         if (state != STATES.PLAYERSEQ) return;
 
         mouseover = true;
         event.target.classList.add('hover');
     });
+
+    // Event for if mouse is no longer over, remove hover tag if true
     colorElement.addEventListener('mouseout', (event) => {
         if (state != STATES.PLAYERSEQ) return;
 
@@ -115,6 +149,13 @@ COLOR_ELEMENTS.forEach(colorElement => {
     });
 });
 
+
+/*
+    Check if the ID of the color clicked matches the color at the current index
+    Return 0 if guess incorrect
+    Return 1 if guess was last in sequence
+    Return -1 if guess was correct
+*/
 function checkSequence(color) {
     if (color !== sequence[num_guesses - 1]) {
         return 0;
@@ -126,9 +167,13 @@ function checkSequence(color) {
     return -1;
 }
 
+
+// Sequence incorrectly matched, game is over
 function gameOver() {
-    state = STATES.GAMEOVER;
-    RESULTS.innerHTML = "Game over! Final score: " + (level - 1);
+    state = STATES.GAMEOVER;    // Change state
+    RESULTS.innerHTML = "Game over! Final score: " + (level - 1);   // Display final score
+
+    // Enable play button
     START_BUTTON.classList.remove('btn-d');
     START_BUTTON.classList.add('btn');
 }
